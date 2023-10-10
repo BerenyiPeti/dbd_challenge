e8c1b4a7f6ef7abc75fea16102cbe92777b33b0c
BerenyiPeti <berenyipeti01@gmail.com>
2023-10-10 18:18:14 +0200

    addonok generálása

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ let challenges = ["no pallet", "no ablak","cserélj ki egy perket no mitherre","
 "no gen ha már csinál valaki", "terror radius = fast vault", "ha valakit chaselnek csak csapatban lehet genezni", 
 "csak healthy csapattárs healelhet"]
 let items = ["kliki kliki", "medkit", "map", "kulcs", "toolbox"]
+let addons = ["addon1", "addon2", "addon3", "addon4"]
 
 let selectedBuild
 let selectedChallenge
 let selectedItem
+let selectedAddon
 
 function roll() {
     generateBuild()
@@ -26,8 +28,20 @@ function generateChallenge() {
 
 function generateItem() {
     generator(items, selectedItem, "item")
+    generateAddons()
 }
 
+function generateAddons() {
+    let add1 = addons[Math.floor(Math.random() * addons.length)]
+    let add2 = addons[Math.floor(Math.random() * addons.length)]
+
+    do {
+        add2 = addons[Math.floor(Math.random() * addons.length)]
+    } while (add1 == add2);
+
+    document.getElementById("addon1").innerHTML = add1
+    document.getElementById("addon2").innerHTML = add2
+}
 
 function generator(array, selected, div) {
     let item = array[Math.floor(Math.random() * array.length)]
@@ -39,5 +53,18 @@ function generator(array, selected, div) {
     } else {
         generator(array, selected, div)
     }
+}
+
+function defaultButton() {
+    document.getElementById("uniqueButton").removeAttribute("disabled")
+    document.getElementById("defaultButton").setAttribute("disabled","")
+    document.getElementById("uniqueDiv").style.display = "none"
+    
+}
+
+function uniqueButton() {
+    document.getElementById("defaultButton").removeAttribute("disabled")
+    document.getElementById("uniqueButton").setAttribute("disabled","")
+    document.getElementById("uniqueDiv").style.display = "block"
 }
 
