a102c8ab3421e6275a390d7a4d37c8a96ed53896
BerenyiPeti <berenyipeti01@gmail.com>
2023-10-10 09:22:34 +0200

    item, generate leegyszerűsítve

--- script.js
+++ script.js
@@ -3,40 +3,41 @@ let builds = ["headon", "láda","4 exhaustion","loopolos", "healer", "genrushos"
 let challenges = ["no pallet", "no ablak","cserélj ki egy perket no mitherre","dobj le minden palletet egyből amint átszaladsz rajta", 
 "no gen ha már csinál valaki", "terror radius = fast vault", "ha valakit chaselnek csak csapatban lehet genezni", 
 "csak healthy csapattárs healelhet"]
+let items = ["kliki kliki", "medkit", "map", "kulcs", "toolbox"]
 
 let selectedBuild
 let selectedChallenge
+let selectedItem
 
 function roll() {
     generateBuild()
     generateChallenge()
+    generateItem()
 }
 
+
 function generateBuild() {
-    let build = builds[randomize(builds.length)]
-    selectedBuild = document.getElementById("build").innerHTML
-
-    if (build != selectedBuild) {
-        document.getElementById("build").innerHTML = build
-    } else {
-        generateBuild()
-    }
-
-
+    generator(builds, selectedBuild, "build")
 }
 
 function generateChallenge() {
-    let challenge = challenges[randomize(challenges.length)]
-    selectedChallenge = document.getElementById("kihivas").innerHTML
+    generator(challenges, selectedChallenge, "kihivas")
+}
 
-    if (challenge != selectedChallenge) {
-        document.getElementById("kihivas").innerHTML = challenge
+function generateItem() {
+    generator(items, selectedItem, "item")
+}
+
+
+function generator(array, selected, div) {
+    let item = array[Math.floor(Math.random() * array.length)]
+
+    selected = document.getElementById(div).innerHTML
+
+    if (item != selected) {
+        document.getElementById(div).innerHTML = item
     } else {
-        generateChallenge()
+        generator(array, selected, div)
     }
 }
 
-function randomize(hossz) {
-    return Math.floor(Math.random() * hossz)
-
-}
